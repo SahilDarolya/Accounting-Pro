efb013fe80c911787055cbaab96780d30bf45cf2
Sahil Darolya <sahilthebuddy@gmail.com>
2024-12-01 22:24:48 +0530

    Fix: Update application configuration for deployment

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
-from app import app, db
+from app import app as application, db
+
+with application.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run()
+    application.run()
